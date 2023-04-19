9bf2f59c3a6dcb34ab38a6440af85a4efc1be8fa
Polina <75391420+wezbicka@users.noreply.github.com>
2023-04-19 16:01:14 +0300

    Оptimized popular posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 from blog.models import Comment, Post, Tag
 
 
@@ -7,7 +8,7 @@ def get_related_posts_count(tag):
 
 
 def get_likes_count(post):
-    return post.likes.count()
+    return post.likes__count
 
 
 def serialize_post(post):
@@ -32,8 +33,10 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.all()
-    most_popular_posts = sorted(posts, key=get_likes_count)[-5:]
+    
+    most_popular_posts = sorted(Post.objects.annotate(
+        Count('likes')
+    ), key=get_likes_count)[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
